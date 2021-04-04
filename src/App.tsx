b3d179b1f67ee3c9c07243bf3d362f7dec83d0ea
RossSumie <rossanasumie@usp.br>
2021-04-04 01:08:39 -0300

    initial version

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { GlobalStyle } from './assets/styles/styles';
+import Main from './pages/Main';
+
+function App() {
+  return (
+    <>  
+      <Main/>
+      <GlobalStyle/>
+    </>
+  );
+}
+
+export default App;
